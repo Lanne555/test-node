a445e49eac92772021aa803ae332ce911f869f32
Lanne555 <Thanakron0063@gmail.com>
2023-12-30 18:45:10 +0700

    add input

--- web_file/cookielogin.js
+++ web_file/cookielogin.js
@@ -1,34 +1,17 @@
-import Swal from 'sweetalert2'
 
-// or via CommonJS
-const Swal = require('sweetalert2')
 
 function process(){
     let usernameinput = document.getElementById(username).value;
     let passwordinput = document.getElementById(password).value;
-    if(username !== "admin" && password !== "00001234"){
-        Swal.fire({
-          title: "Password",
-          text: "password is correct",
-          icon: "success"
-        });
+    alert(usernameinput);
+    alert(passwordinput);
+    if(usernameinput == "admin" && passwordinput == "00001234"){
+        alert("pass");
     }
     else{
-        Swal.fire({
-          title: "Password",
-          text: "password is incorrect",
-          icon: "error"
-        }); 
+        alert("failled");
     }
 }
 
-
-
-
-
-function ccookie() {
-    // code 
-    document.cookie = "typeuser=1";
-}
 
 
