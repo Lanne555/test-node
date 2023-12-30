aff9bf1ff7dbdf3ba03162a8fddb7c2d5479433a
Lanne555 <Thanakron0063@gmail.com>
2023-12-30 18:42:07 +0700

    erorr sweetalert2

--- web_file/cookielogin.js
+++ web_file/cookielogin.js
@@ -1,16 +1,34 @@
+import Swal from 'sweetalert2'
+
+// or via CommonJS
+const Swal = require('sweetalert2')
+
+function process(){
+    let usernameinput = document.getElementById(username).value;
+    let passwordinput = document.getElementById(password).value;
+    if(username !== "admin" && password !== "00001234"){
+        Swal.fire({
+          title: "Password",
+          text: "password is correct",
+          icon: "success"
+        });
+    }
+    else{
+        Swal.fire({
+          title: "Password",
+          text: "password is incorrect",
+          icon: "error"
+        }); 
+    }
+}
+
+
 
 
 
 function ccookie() {
     // code 
-    document.cookie="typeuser=1";
-    var cokiee = "cookie1";
-    document.getElementById("cookie1").innerHTML = cokiee;
-  }
+    document.cookie = "typeuser=1";
+}
 
-function dcookie() {
-    // code 
-    document.cookie="typeuser=0";
-    var cokiee = "cookie0";
-    document.getElementById("cookie1").innerHTML = cokiee;
-  }
+
