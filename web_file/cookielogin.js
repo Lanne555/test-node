bb1b7b882ec2a22b8d22c3494c73fecd284d5531
Lanne555 <Thanakron0063@gmail.com>
2023-12-30 17:56:19 +0700

    first cookie

--- web_file/cookielogin.js
+++ web_file/cookielogin.js
@@ -1,18 +1,16 @@
-var cookie1 = document.getElementsByClassName("cookie");
-var btnc = document.getElementsByClassName("btnc");
-var btnd = document.getElementsByClassName("btnd");
+
 
 
 function ccookie() {
     // code 
     document.cookie="typeuser=1";
-    var cokie = 1;
-    document.getElementById("cookie").innerHTML = "cookie is on";
+    var cokiee = "cookie1";
+    document.getElementById("cookie1").innerHTML = cokiee;
   }
 
 function dcookie() {
     // code 
     document.cookie="typeuser=0";
-    var cokie = 0;
-    document.getElementById("cookie").innerHTML = "cookie is off";
+    var cokiee = "cookie0";
+    document.getElementById("cookie1").innerHTML = cokiee;
   }
